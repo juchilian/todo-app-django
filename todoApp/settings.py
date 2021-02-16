07ba63f2976d0ad67167616c2a3124e00d3bd992
juchilian <juchilian@outlook.jp>
2021-02-16 21:48:18 +0900

    [update]

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@7w^un+n4m0a_b=e@ev+6!v9jy*i=#&k4&0&dti74bkdg7a#qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
